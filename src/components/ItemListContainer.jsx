0893bd2ddc1b1d89078ed77996f31352d0d8cef9
FacuVasquez <Facuvasquez@outlook.com>
2024-02-16 10:21:08 -0300

    arrelgo de pages2

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,8 @@
-const ItemListContainer = ({ greeting }) => {
+const ItemListContainer = ({greeting}) => {
     return (
       <div className="item-list-container">
         <h2>{greeting}</h2>
-        {/* Aquí puedes agregar más contenido según sea necesario */}
+        {}
       </div>
     );
   };
